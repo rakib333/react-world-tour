60d072e7311f8a60cbc6e3cc1c803186a972a1af
sm rakib <smrakib333@gmail.com>
2024-06-04 00:50:54 +0600

    initial commit

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import './Country.css';
+
+const Country = ({country}) => {
+    
+    const {name, population, area, flags} = country;
+    const [visited, setVisited] = useState(false);
+    const handleVisit = () => {
+        setVisited(!visited)
+    }
+
+    return (
+        <div className="country">
+            <h2>{name.common}</h2>
+            <img src={flags.png} alt="" />
+            <p>Population: {population}</p>
+            <p>Area: {area}</p>
+            <button onClick={handleVisit}>{visited ? "Visited" : "Not visited"}</button>
+            {visited ? "I have visited this country" : "I want to visit."}
+        </div>
+    );
+};
+
+export default Country;
